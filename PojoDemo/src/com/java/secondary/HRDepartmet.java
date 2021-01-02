f1723547fc2d15716e0b6180e082d45a21ff8ae3
prajakta28jadhav <prajakta28jadhav@gmail.com>
2021-01-02 14:34:44 +0530

    added

--- PojoDemo/src/com/java/secondary/HRDepartmet.java
+++ PojoDemo/src/com/java/secondary/HRDepartmet.java
@@ -4,7 +4,6 @@ public class HRDepartmet {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		System.out.println("Member 2 added");
-		System.out.println("MEMBER1 ADDED");
-		System.out.println("Member2 Added");
+		
 	}
 }
